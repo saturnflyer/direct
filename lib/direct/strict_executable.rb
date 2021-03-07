8dca084c3cc7fb8b11a1c42490e8f871cdef5d49
Jim Gay <jim@saturnflyer.com>
2021-03-07 08:58:14 -0500

    Set StrictExecutable to inherit from Executable

--- lib/direct/strict_executable.rb
+++ lib/direct/strict_executable.rb
@@ -1,5 +1,5 @@
 module Direct
-  class StrictExecutable
+  class StrictExecutable < Executable
     include Direct
 
     # It is intended that you initialize objects via Direct.strict_defer
@@ -42,62 +42,6 @@ module Direct
     #      puts "#{thing} failed!"
     #   }.execute
     #
-    def initialize(callable=nil, object: nil, &block)
-      @object = object
-      @execution = callable || block
-      @exception_classes = [StandardError]
-    end
-    attr_reader :execution, :exception_classes, :object
-
-    # Tell the object what to do for a success path
-    #
-    # Returns itself
-    #
-    def success(callable=nil, &block)
-      direct(:success, (callable || block))
-      self
-    end
-
-    # Tell the object what to do for a failure path
-    #
-    # Returns itself
-    #
-    def failure(callable=nil, &block)
-      direct(:failure, (callable || block))
-      self
-    end
-
-    # Tell the object what to do for an exception path.
-    #
-    # You may optionally provide a list of modules rescued by
-    # the value method in the case of an exception.
-    #
-    # Returns itself
-    #
-    # Example:
-    #
-    #   Direct.strict_defer {
-    #      # something...
-    #   }.exception(NoMethodError) { |deferred, exception, object|
-    #      ExceptionNotifier.notify(exception)
-    #   }
-    #
-    def exception(*classes, &block)
-      unless classes.empty?
-        @exception_classes = classes
-      end
-      direct(:exception, block)
-      self
-    end
-
-    def run_exception_block(exception)
-      if __directions.key?(:exception)
-        as_directed(:exception, exception, object)
-      else
-        as_directed(:failure, exception, object)
-      end
-    end
-    private :run_exception_block
 
     # Return the value of the success or failure path
     # and rescue from StandardError or from the modules
