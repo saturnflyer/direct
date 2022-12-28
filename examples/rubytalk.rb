08b5d77c4d607f9759ed1e3ace58c6fcb0a45ac6
Jim Gay <jim@saturnflyer.com>
2022-12-27 23:31:30 -0500

    Add exception to example

--- examples/rubytalk.rb
+++ examples/rubytalk.rb
@@ -5,6 +5,7 @@ class Operation
 
   def initialize(&block)
     @block = block
+    @exception_handler = Direct::ExceptionHandler.new
   end
 
   def if_true(&)
@@ -15,12 +16,19 @@ class Operation
     direct(:if_false, &)
   end
 
+  def if_exception(type = RuntimeError, &)
+    @exception_handler.monitor(type, &)
+    direct(:if_exception, @exception_handler)
+  end
+
   def call
     if @block.call
       as_directed(:if_true)
     else
       as_directed(:if_false)
     end
+  rescue *@exception_handler.classes => e
+    as_directed(:if_exception, e, self)
   end
 end
 
@@ -31,4 +39,5 @@ end
 Operation { [true, false].sample }
   .if_true { puts "yay!" }
   .if_false { puts "boo!" }
+  .if_exception { puts "oops!" }
   .call
